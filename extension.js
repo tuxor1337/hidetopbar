a9ec757fd4f1b216fe4791a5b672e2c2302a3763
Philip Witte <witte2008@gmail.com>
2012-12-18 15:20:22 -0500

    Issue #2 : fix maximized window glitch when going to/from overview mode in gnome-shell 3.6

--- extension.js
+++ extension.js
@@ -6,65 +6,54 @@
 // http://www.fpmurphy.com/gnome-shell-extensions/
 //
 
-const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 
-let text, button;
-
-// change these to suit your own tastes and your system
-const PANEL_HEIGHT = 25;
+let PANEL_HEIGHT = 25;
 const AUTOHIDE_ANIMATION_TIME = 0.2;
-const TIME_DELTA = 1500;
 
 function _hideTopPanel() {
-    Tweener.addTween(Main.panel.actor,
-                    { height: 1,
-                       time: AUTOHIDE_ANIMATION_TIME,
-                       transition: 'easeOutQuad'
-                     });
+    Main.panel.actor.set_height(1);
+    Main.panel._leftCorner.actor.set_y(0);
+    Main.panel._rightCorner.actor.set_y(0);
 
-    let params = { y: 0,
-                   time: AUTOHIDE_ANIMATION_TIME,
-                   transition: 'easeOutQuad'
-                 };
-
-    Tweener.addTween(Main.panel._leftCorner.actor, params);
-    Tweener.addTween(Main.panel._rightCorner.actor, params);
-
-    params = { opacity: 0,
-               time: AUTOHIDE_ANIMATION_TIME - 0.1,
-               transition: 'easeOutQuad'
-             };
-
-    Tweener.addTween(Main.panel._leftBox, params);
-    Tweener.addTween(Main.panel._centerBox, params);
-    Tweener.addTween(Main.panel._rightBox, params);
+    Main.panel._leftBox.set_opacity(0);
+    Main.panel._centerBox.hide();
+    Main.panel._rightBox.hide();
 }
 
 function _showTopPanel() {
-    let params = { y: PANEL_HEIGHT - 1,
-                   time: AUTOHIDE_ANIMATION_TIME + 0.1,
-                   transition: 'easeOutQuad'
-                 };
+    Tweener.addTween(Main.panel.actor, {
+        height: PANEL_HEIGHT,
+        time: AUTOHIDE_TIME,
+        transition: 'easeOutQuad',
+        onComplete: function() {
+            Main.panel._centerBox.show();
+            Main.panel._centerBox.set_opacity(0);
+
+            Main.panel._rightBox.show();
+            Main.panel._rightBox.set_opacity(0);
+
+            let boxParams = {
+                opacity: 255,
+                time: 0.2,
+                transition: 'easeOutQuad'
+            };
+
+            Tweener.addTween(Main.panel._leftBox, boxParams);
+            Tweener.addTween(Main.panel._centerBox, boxParams);
+            Tweener.addTween(Main.panel._rightBox, boxParams);
+        }
+    });
+
+    let params = {
+        y: PANEL_HEIGHT - 1,
+        time: AUTOHIDE_TIME,
+        transition: 'easeOutQuad'
+    };
 
     Tweener.addTween(Main.panel._leftCorner.actor, params);
     Tweener.addTween(Main.panel._rightCorner.actor, params);
-
-    Tweener.addTween(Main.panel.actor,
-                 { height: PANEL_HEIGHT,
-                   time: AUTOHIDE_ANIMATION_TIME,
-                   transition: 'easeOutQuad'
-                 });
-
-    params = { opacity: 255,
-               time: AUTOHIDE_ANIMATION_TIME+0.2,
-               transition: 'easeOutQuad'
-             };
-
-    Tweener.addTween(Main.panel._leftBox, params);
-    Tweener.addTween(Main.panel._centerBox, params);
-    Tweener.addTween(Main.panel._rightBox, params);
 }
 
 function init() {
