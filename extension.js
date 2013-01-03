60650f7c2ed998e03f3802bfa5e2b65d55c02dcb
Mathieu Lutfy <mathieu@bidon.ca>
2013-01-02 21:43:09 -0500

    Convert to a 'prototype' based on fpmurphy's code + Thomas Vogt's feedback, so that we can properly disconnect the events when the extension is disabled.

--- extension.js
+++ extension.js
@@ -1,10 +1,12 @@
 //
-// Hides Gnome's topbar except in overview mode.
+// Hides the Gnome "top bar" except in overview mode.
+// https://extensions.gnome.org/extension/545/hide-top-bar/
 // https://github.com/mlutfy/hidetopbar
-// http://www.bidon.ca
 //
 // Based on Finnbarr P. Murphy's extension "autohidetopbar@fpmurphy.com"
 // http://www.fpmurphy.com/gnome-shell-extensions/
+//
+// See README for more information.
 //
 
 const Main = imports.ui.main;
@@ -13,64 +15,81 @@ const Tweener = imports.ui.tweener;
 let PANEL_HEIGHT = 25;
 const AUTOHIDE_TIME = 0.2;
 
-function _hideTopPanel() {
-    Main.panel.actor.set_height(1);
-    Main.panel._leftCorner.actor.set_y(0);
-    Main.panel._rightCorner.actor.set_y(0);
-
-    Main.panel._leftBox.set_opacity(0);
-    Main.panel._centerBox.hide();
-    Main.panel._rightBox.hide();
+function HideTopPanel() {
+    this._init();
 }
 
-function _showTopPanel() {
-    Tweener.addTween(Main.panel.actor, {
-        height: PANEL_HEIGHT,
-        time: AUTOHIDE_TIME,
-        transition: 'easeOutQuad',
-        onComplete: function() {
-            Main.panel._centerBox.show();
-            Main.panel._centerBox.set_opacity(0);
+HideTopPanel.prototype = {
+    _init: function() {
+        this._shownEvent = 0;
+        this._hidingEvent = 0;
+    },
 
-            Main.panel._rightBox.show();
-            Main.panel._rightBox.set_opacity(0);
+    _hideTopPanel: function() {
+        Main.panel.actor.set_height(1);
+        Main.panel._leftCorner.actor.set_y(0);
+        Main.panel._rightCorner.actor.set_y(0);
+    
+        Main.panel._leftBox.set_opacity(0);
+        Main.panel._centerBox.hide();
+        Main.panel._rightBox.hide();
+    },
+    
+    _showTopPanel: function() {
+        Tweener.addTween(Main.panel.actor, {
+            height: PANEL_HEIGHT,
+            time: AUTOHIDE_TIME,
+            transition: 'easeOutQuad',
+            onComplete: function() {
+                Main.panel._centerBox.show();
+                Main.panel._centerBox.set_opacity(0);
+    
+                Main.panel._rightBox.show();
+                Main.panel._rightBox.set_opacity(0);
+    
+                let boxParams = {
+                    opacity: 255,
+                    time: 0.2,
+                    transition: 'easeOutQuad'
+                };
+    
+                Tweener.addTween(Main.panel._leftBox, boxParams);
+                Tweener.addTween(Main.panel._centerBox, boxParams);
+                Tweener.addTween(Main.panel._rightBox, boxParams);
+            }
+        });
+    
+        let params = {
+            y: PANEL_HEIGHT - 1,
+            time: AUTOHIDE_TIME,
+            transition: 'easeOutQuad'
+        };
+    
+        Tweener.addTween(Main.panel._leftCorner.actor, params);
+        Tweener.addTween(Main.panel._rightCorner.actor, params);
+    },
+    
+    enable: function() {
+        this._hideTopPanel();
+    
+        this._shownEvent = Main.overview.connect('shown', this._showTopPanel);
+        this._hidingEvent = Main.overview.connect('hiding', this._hideTopPanel);
+    },
+    
+    disable: function() {
+        this._showTopPanel();
 
-            let boxParams = {
-                opacity: 255,
-                time: 0.2,
-                transition: 'easeOutQuad'
-            };
-
-            Tweener.addTween(Main.panel._leftBox, boxParams);
-            Tweener.addTween(Main.panel._centerBox, boxParams);
-            Tweener.addTween(Main.panel._rightBox, boxParams);
+        if (this._shownEvent) {
+            Main.overview.disconnect(this._shownEvent);
         }
-    });
 
-    let params = {
-        y: PANEL_HEIGHT - 1,
-        time: AUTOHIDE_TIME,
-        transition: 'easeOutQuad'
-    };
-
-    Tweener.addTween(Main.panel._leftCorner.actor, params);
-    Tweener.addTween(Main.panel._rightCorner.actor, params);
+        if (this._hidingEvent) {
+            Main.overview.disconnect(this._hidingEvent);
+        }
+    }
 }
 
 function init() {
-}
-
-function enable() {
-    _hideTopPanel();
-
-    Main.overview.connect('shown', _showTopPanel);
-    Main.overview.connect('hiding', _hideTopPanel);
-}
-
-function disable() {
-    _showTopPanel();
-
-    Main.overview.disconnect(_showTopPanel);
-    Main.overview.disconnect(_hideTopPanel);
+    return new HideTopPanel();
 }
 
