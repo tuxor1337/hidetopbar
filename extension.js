50b4a4c8ce673cb5faec6dc95859ca3581b60f36
Thomas Vogt <tuxor1337@web.de>
2013-02-21 02:48:57 +0100

    Complete rewrite and implementation of option added in 881f993

--- extension.js
+++ extension.js
@@ -3,118 +3,96 @@
 // https://extensions.gnome.org/extension/545/hide-top-bar/
 // https://github.com/mlutfy/hidetopbar
 //
-// Based on Finnbarr P. Murphy's extension "autohidetopbar@fpmurphy.com"
-// http://www.fpmurphy.com/gnome-shell-extensions/
-//
 // See README for more information.
 //
 
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
-
+const Lang = imports.lang;
 const Settings = imports.misc.extensionUtils.getCurrentExtension()
                     .imports.convenience.getSettings();
-const SETTINGS_HOT_CORNER = 'hot-corner';
-
+                    
 const PANEL_BOX = Main.panel.actor.get_parent();
 const PANEL_HEIGHT = PANEL_BOX.get_height();
-const ANIMATION_TIME = 0.2;
+const ANIMATION_TIME_OVERVIEW = 0.4;
+const ANIMATION_TIME_AUTOHIDE = 0.2;
 
-function HideTopPanel() {
-    this._init();
+let _showEvent = 0;
+let _hideEvent = 0;
+let _stgsEvent = 0;
+let _stgsEvent2 = 0;
+let _enterEvent = 0;
+let _leaveEvent = 0;
+
+function _hidePanel(animationTime) {
+    let x = Number(Settings.get_boolean('hot-corner'))
+    PANEL_BOX.height = x;
+    Tweener.addTween(Main.panel.actor, {
+        y: x-PANEL_HEIGHT,
+        time: animationTime,
+        transition: 'easeOutQuad'
+    });
 }
 
-HideTopPanel.prototype = {
-    _hideTopPanel: function() {
-        if(Settings.get_boolean(SETTINGS_HOT_CORNER)) PANEL_BOX.height=1;
-        else PANEL_BOX.height=0;
-        Tweener.addTween(Main.panel.actor, {
-            y: -PANEL_HEIGHT+1,
-            time: ANIMATION_TIME + 0.2,
-            transition: 'easeOutQuad',
-            onComplete: function() {
-                Main.panel.actor.height = 1;
-                Main.panel.actor.y = 0;
+function _showPanel(animationTime) {
+    Tweener.addTween(Main.panel.actor, {
+        y: 0,
+        time: animationTime,
+        transition: 'easeOutQuad'
+    });
+    
+    PANEL_BOX.height = PANEL_HEIGHT;
+}
+
+function _toggleMouseSensitive() {
+    if(Settings.get_boolean('mouse-sensitive')) {
+        _enterEvent = Main.panel.actor.connect('enter-event', function() {
+            _showPanel(ANIMATION_TIME_AUTOHIDE); });
+        _leaveEvent = Main.panel.actor.connect('leave-event', function tmpFct() { 
+            if(!Main.overview.visible) {
+                blocker = (Main.panel._menus || Main.panel.menuManager)._activeMenu
+                if(blocker == null) {
+                    _hidePanel(ANIMATION_TIME_AUTOHIDE);
+                } else {
+                    blocker.connect('open-state-changed', function(menu, open){
+                        if(!open) tmpFct();
+                    });
+                }
             }
         });
-
-        let params = { 
-            y: 0,
-            time: ANIMATION_TIME + 0.2,
-            transition: 'easeOutQuad'
-        };
- 
-        Tweener.addTween(Main.panel._leftCorner.actor, params);
-        Tweener.addTween(Main.panel._rightCorner.actor, params);
-
-        params = {
-            opacity: 0,
-            time: ANIMATION_TIME,
-            transition: 'easeOutQuad'
-        };
-
-        Tweener.addTween(Main.panel._leftBox, params);
-        Tweener.addTween(Main.panel._centerBox, params);
-        Tweener.addTween(Main.panel._rightBox, params);
-    },
-    
-    _showTopPanel: function(po) {
-        Main.panel.actor.y = -PANEL_HEIGHT+1;
-        Main.panel.actor.height = PANEL_HEIGHT;
-        Tweener.addTween(Main.panel.actor, {
-            y: 0,
-            time: ANIMATION_TIME + 0.2,
-            transition: 'easeOutQuad'
-        });
-
-        let params = {
-            opacity: 255,
-            time: ANIMATION_TIME + 0.2,
-            transition: 'easeOutQuad'
-        };
-
-        Tweener.addTween(Main.panel._leftBox, params);
-        Tweener.addTween(Main.panel._centerBox, params);
-        Tweener.addTween(Main.panel._rightBox, params);
-    
-        let params = {
-            y: PANEL_HEIGHT - 1,
-            time: ANIMATION_TIME,
-            transition: 'easeOutQuad'
-        };
-    
-        Tweener.addTween(Main.panel._leftCorner.actor, params);
-        Tweener.addTween(Main.panel._rightCorner.actor, params);
-        
-        Main.overview._relayout();
-    },
-    
-    _init: function() {
-        this._showEvent = 0;
-        this._hideEvent = 0;
-        this._stgsEvent = 0;
-    },
-    
-    enable: function() {
-        this._showEvent = Main.overview.connect('showing', this._showTopPanel);
-        this._hideEvent = Main.overview.connect('hiding', this._hideTopPanel);
-        this._stgsEvent = Settings.connect('changed::' + SETTINGS_HOT_CORNER,
-                                           this._hideTopPanel)
-        
-        this._hideTopPanel();
-    },
-    
-    disable: function() {
-        if(this._showEvent) Main.overview.disconnect(this._showEvent);
-        if(this._hideEvent) Main.overview.disconnect(this._hideEvent);
-        if(this._stgsEvent) Settings.disconnect(this._stgsEvent);
-
-        this._showTopPanel();
-        PANEL_BOX.height = PANEL_HEIGHT;
+    } else {
+        if(_enterEvent) Main.panel.actor.disconnect(_enterEvent);
+        if(_leaveEvent) Main.panel.actor.disconnect(_leaveEvent);
     }
-};
+}
 
-function init() {
-    return new HideTopPanel();
+function init() { }
+
+function enable() {
+    Main.layoutManager.removeChrome(PANEL_BOX);
+    Main.layoutManager.addChrome(PANEL_BOX, { affectsStruts: false });
+    
+    _showEvent = Main.overview.connect('showing', function() { _showPanel(ANIMATION_TIME_OVERVIEW); });
+    _hideEvent = Main.overview.connect('hiding', function() { _hidePanel(ANIMATION_TIME_OVERVIEW); });
+    _stgsEvent = Settings.connect('changed::hot-corner', function() { _hidePanel(0.1); });
+    
+    _stgsEvent2 = Settings.connect('changed::mouse-sensitive', _toggleMouseSensitive);
+    _toggleMouseSensitive();
+    
+    _hidePanel(0.1);
+}
+
+function disable() {
+    Main.layoutManager.removeChrome(PANEL_BOX);
+    Main.layoutManager.addChrome(PANEL_BOX, { affectsStruts: true});
+    
+    if(_showEvent) Main.overview.disconnect(_showEvent);
+    if(_hideEvent) Main.overview.disconnect(_hideEvent);
+    if(_stgsEvent) Settings.disconnect(_stgsEvent);
+    if(_stgsEvent2) Settings.disconnect(_stgsEvent2);
+    if(_enterEvent) Main.panel.actor.disconnect(_enterEvent);
+    if(_leaveEvent) Main.panel.actor.disconnect(_leaveEvent);
+    
+    _showPanel(0.1);
 }
 
