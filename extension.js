573dad08d8595b74f1e6131fcbe7ff5eb9510ab3
tuxor1337 <tuxor1337@web.de>
2013-04-27 11:16:36 +0300

    Attempt producing smoother animation for 3.8.

--- extension.js
+++ extension.js
@@ -15,8 +15,12 @@ const PANEL_ACTOR = Main.panel.actor;
 const PANEL_BOX = PANEL_ACTOR.get_parent();
 
 let _panelHeight = PANEL_ACTOR.get_height();
+
 let _showEvent = 0;
 let _hideEvent = 0;
+
+let _stgsAnimEvent = 0;
+let _stgsAnimEvent2 = 0;
 let _stgsEvent = 0;
 let _stgsEvent2 = 0;
 
@@ -25,12 +29,16 @@ let _leaveEvent = 0;
 let _menuEvent = 0;
 let _blockerMenu = 0;
 
+let _settingsHotCorner = Settings.get_boolean('hot-corner');
+let _settingsMouseSensitive = Settings.get_boolean('mouse-sensitive');
+let _settingsAnimTimeOverv = Settings.get_double('animation-time-overview');
+let _settingsAnimTimeAutoh = Settings.get_double('animation-time-autohide');
+
 function _hidePanel(animationTime) {
     /* Still looking for some kind of "size-changed" event, see issue #12. */
     _panelHeight = PANEL_ACTOR.get_height();
     
-    let hotCornerSetting = Settings.get_boolean('hot-corner');
-    let x = Number(hotCornerSetting)
+    let x = Number(_settingsHotCorner)
     PANEL_BOX.height = x;
     
     Tweener.addTween(PANEL_ACTOR, {
@@ -75,7 +83,7 @@ function _handleMenus() {
     if(!Main.overview.visible) {
         blocker = (Main.panel._menus || Main.panel.menuManager)._activeMenu
         if(blocker == null) {
-            _hidePanel(Settings.get_double('animation-time-autohide'));
+            _hidePanel(_settingsAnimTimeAutoh);
         } else {
             _blockerMenu = blocker
             _menuEvent = _blockerMenu.connect('open-state-changed', function(menu, open){
@@ -90,9 +98,10 @@ function _handleMenus() {
 }
 
 function _toggleMouseSensitive() {
-    if(Settings.get_boolean('mouse-sensitive')) {
+    _settingsMouseSensitive = Settings.get_boolean('mouse-sensitive');
+    if(_settingsMouseSensitive) {
         _enterEvent = PANEL_ACTOR.connect('enter-event', function() {
-            _showPanel(Settings.get_double('animation-time-autohide'));
+            _showPanel(_settingsAnimTimeAutoh);
         });
         _leaveEvent = PANEL_ACTOR.connect('leave-event', _handleMenus);
     } else {
@@ -108,12 +117,24 @@ function enable() {
     Main.layoutManager.addChrome(PANEL_BOX, { affectsStruts: false });
     
     _showEvent = Main.overview.connect('showing', function() {
-        _showPanel(Settings.get_double('animation-time-overview'));
+        _showPanel(_settingsAnimTimeOverv);
     });
     _hideEvent = Main.overview.connect('hiding', function() {
-        _hidePanel(Settings.get_double('animation-time-overview'));
+        _hidePanel(_settingsAnimTimeOverv);
     });
-    _stgsEvent = Settings.connect('changed::hot-corner', function() { _hidePanel(0.1); });
+    
+    _stgsAnimEvent = Settings.connect('changed::animation-time-overview',
+        function() { 
+            _settingsAnimTimeOverv = Settings.get_double('animation-time-overview');
+    });
+    _stgsAnimEvent2 = Settings.connect('changed::animation-time-autohide',
+        function() { 
+            _settingsAnimTimeAutoh = Settings.get_double('animation-time-autohide');
+    });
+    _stgsEvent = Settings.connect('changed::hot-corner', function() { 
+        _settingsHotCorner = Settings.get_boolean('hot-corner');
+        _hidePanel(0.1);
+    });
     
     _stgsEvent2 = Settings.connect('changed::mouse-sensitive', _toggleMouseSensitive);
     _toggleMouseSensitive();
@@ -127,6 +148,8 @@ function disable() {
     
     if(_showEvent) Main.overview.disconnect(_showEvent);
     if(_hideEvent) Main.overview.disconnect(_hideEvent);
+    if(_stgsAnimEvent) Settings.disconnect(_stgsAnimEvent);
+    if(_stgsAnimEvent2) Settings.disconnect(_stgsAnimEvent2);
     if(_stgsEvent) Settings.disconnect(_stgsEvent);
     if(_stgsEvent2) Settings.disconnect(_stgsEvent2);
     if(_enterEvent) PANEL_ACTOR.disconnect(_enterEvent);
