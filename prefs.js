66dcb6f628fad9724b7a61d8319fd8a5ddb11d6f
Thomas Vogt <thomas.vogt@uni-luebeck.de>
2017-04-11 09:33:44 +0200

    Correct use of  variable declarations, fixes #128.

--- prefs.js
+++ prefs.js
@@ -16,8 +16,21 @@ function init() {
 }
 
 function buildPrefsWidget() {
-    let frame = new Gtk.VBox({border_width: 10, spacing: 6});
-    
+    let frame = new Gtk.VBox({border_width: 10, spacing: 6}),
+        settings_vbox,
+        settings_array,
+        settings_onoff,
+        settings_hbox,
+        settings_spin,
+        model,
+        model_row,
+        binding,
+        binding_key,
+        binding_mods,
+        treeview,
+        treeview_col,
+        cellrend;
+
 /******************************************************************************
  ************************************** Section Sensitivity *******************
  ******************************************************************************/
@@ -28,62 +41,60 @@ function buildPrefsWidget() {
         xalign: 0
     }), false, false, 0);
 
-    let settings_vbox = new Gtk.VBox({margin_left: 20, margin_top: 10, spacing: 6});
-    let settings_array = [
+    settings_vbox = new Gtk.VBox({margin_left: 20, margin_top: 10, spacing: 6});
+    settings_array = [
         ['mouse-sensitive',_("Show panel when mouse approaches edge of the screen")],
         ['hot-corner',_("Keep hot corner sensitive, even in hidden state")],
         ['mouse-triggers-overview',_("In the above case show overview, too")],
     ];
     settings_array.forEach(function (s) {
-        let hbox = new Gtk.HBox();
-        let onoff = new Gtk.Switch({active: settings.get_boolean(s[0])});
-        
-        hbox.pack_start(new Gtk.Label({
+        settings_onoff = new Gtk.Switch({active: settings.get_boolean(s[0])});
+        settings_onoff.connect('notify::active', function(w) {
+            settings.set_boolean(s[0], w.active);
+        });
+        settings.connect('changed::'+s[0], function(k,b) {
+            settings_onoff.set_active(settings.get_boolean(b));
+        });
+
+        settings_hbox = new Gtk.HBox();
+        settings_hbox.pack_start(new Gtk.Label({
             label: s[1],
             use_markup: true,
             xalign: 0
         }), true, true, 0);
-        hbox.pack_end(onoff, false, false, 0);
-        
-        settings.connect('changed::'+s[0], function(k,b) {
-            onoff.set_active(settings.get_boolean(b)); });
+        settings_hbox.pack_end(settings_onoff, false, false, 0);
 
-        onoff.connect('notify::active', function(w) {
-            settings.set_boolean(s[0], w.active);
-        });
-        
-        settings_vbox.pack_start(hbox, false,false, 0);
+        settings_vbox.pack_start(settings_hbox, false,false, 0);
     });
     frame.pack_start(settings_vbox, true, true, 0);
 
-    let settings_vbox = new Gtk.VBox({margin_left: 20, margin_bottom: 10, spacing: 6});
-    let settings_array = [
+    settings_vbox = new Gtk.VBox({margin_left: 20, margin_bottom: 10, spacing: 6});
+    settings_array = [
         ['pressure-threshold',_("Pressure barrier's threshold.")],
         ['pressure-timeout',_("Pressure barrier's timeout.")]
     ];
     settings_array.forEach(function (s) {
-        let hbox = new Gtk.HBox();
-        let spin = Gtk.SpinButton.new_with_range(0,10000,1);
-        spin.set_value(settings.get_int(s[0]));
-        
-        hbox.pack_start(new Gtk.Label({
+        settings_spin = Gtk.SpinButton.new_with_range(0,10000,1);
+        settings_spin.set_value(settings.get_int(s[0]));
+        settings_spin.connect('value-changed', function(w) {
+            settings.set_int(s[0], w.get_value());
+        });
+        settings.connect('changed::'+s[0], function(k,b) {
+            settings_spin.set_value(settings.get_int(b));
+        });
+
+        settings_hbox = new Gtk.HBox();
+        settings_hbox.pack_start(new Gtk.Label({
             label: s[1],
             use_markup: true,
             xalign: 0
         }), true, true, 0);
-        hbox.pack_end(spin, false, false, 0);
-        
-        settings.connect('changed::'+s[0], function(k,b) {
-            spin.set_value(settings.get_int(b)); });
+        settings_hbox.pack_end(settings_spin, false, false, 0);
 
-        spin.connect('value-changed', function(w) {
-            settings.set_int(s[0], w.get_value());
-        });
-        
-        settings_vbox.pack_start(hbox, false,false, 0);
+        settings_vbox.pack_start(settings_hbox, false,false, 0);
     });
     frame.pack_start(settings_vbox, true, true, 0);
-    
+
 /******************************************************************************
  ************************************** Section Animation *********************
  ******************************************************************************/
@@ -94,34 +105,33 @@ function buildPrefsWidget() {
         xalign: 0
     }), false, false, 0);
 
-    let settings_vbox = new Gtk.VBox({margin_left: 20, margin_top: 10, margin_bottom: 10, spacing: 6});
-    let settings_array = [
+    settings_vbox = new Gtk.VBox({margin_left: 20, margin_top: 10, margin_bottom: 10, spacing: 6});
+    settings_array = [
         ['animation-time-overview',_("Slide animation time when entering/leaving overview.")],
         ['animation-time-autohide',_("Slide animation time when mouse approaches edge of the screen.")]
     ];
     settings_array.forEach(function (s) {
-        let hbox = new Gtk.HBox();
-        let spin = Gtk.SpinButton.new_with_range(0.0,1.0,0.1);
-        spin.set_value(settings.get_double(s[0]));
-        
-        hbox.pack_start(new Gtk.Label({
+        settings_spin = Gtk.SpinButton.new_with_range(0.0,1.0,0.1);
+        settings_spin.set_value(settings.get_double(s[0]));
+        settings_spin.connect('value-changed', function(w) {
+            settings.set_double(s[0], w.get_value());
+        });
+        settings.connect('changed::'+s[0], function(k,b) {
+            settings_spin.set_value(settings.get_double(b));
+        });
+
+        settings_hbox = new Gtk.HBox();
+        settings_hbox.pack_start(new Gtk.Label({
             label: s[1],
             use_markup: true,
             xalign: 0
         }), true, true, 0);
-        hbox.pack_end(spin, false, false, 0);
-        
-        settings.connect('changed::'+s[0], function(k,b) {
-            spin.set_value(settings.get_double(b)); });
+        settings_hbox.pack_end(settings_spin, false, false, 0);
 
-        spin.connect('value-changed', function(w) {
-            settings.set_double(s[0], w.get_value());
-        });
-        
-        settings_vbox.pack_start(hbox, false,false, 0);
+        settings_vbox.pack_start(settings_hbox, false,false, 0);
     });
     frame.pack_start(settings_vbox, true, true, 0);
-    
+
 /******************************************************************************
  ************************************** Section Shortcuts *********************
  ******************************************************************************/
@@ -132,46 +142,45 @@ function buildPrefsWidget() {
         xalign: 0
     }), false, false, 0);
 
-    let settings_vbox = new Gtk.VBox({margin_left: 20, margin_top: 10, spacing: 6});
-    
+    settings_vbox = new Gtk.VBox({margin_left: 20, margin_top: 10, spacing: 6});
+
 /* ++++++++++++++++++++++++++++++++++++ Keyboard accelerator +++++ */
 
-    let hbox = new Gtk.HBox();
-    let model = new Gtk.ListStore();
+    settings_hbox = new Gtk.HBox();
+    model = new Gtk.ListStore();
 
     model.set_column_types([
         GObject.TYPE_INT,
         GObject.TYPE_INT
     ]);
-    let row = model.append();
-    let binding = settings.get_strv('shortcut-keybind')[0];
-    let key, mods;
+    model_row = model.append();
+    binding = settings.get_strv('shortcut-keybind')[0];
     if (binding) {
-        [key, mods] = Gtk.accelerator_parse(binding);
+        [binding_key, binding_mods] = Gtk.accelerator_parse(binding);
     } else {
-        [key, mods] = [0, 0];
+        [binding_key, binding_mods] = [0, 0];
     }
-    model.set(row, [0, 1], [mods, key]);
+    model.set(model_row, [0, 1], [binding_mods, binding_key]);
 
-    let treeview = new Gtk.TreeView({ 'expand': false, 'model': model });
-    let cellrend = new Gtk.CellRendererAccel({
+    treeview = new Gtk.TreeView({ 'expand': false, 'model': model });
+    cellrend = new Gtk.CellRendererAccel({
         'editable': true,
         'accel-mode': Gtk.CellRendererAccelMode.GTK
     });
 
-    cellrend.connect('accel-edited', function(rend, iter, key, mods) {
-        let value = Gtk.accelerator_name(key, mods);
+    cellrend.connect('accel-edited', function(rend, iter, binding_key, binding_mods) {
+        let value = Gtk.accelerator_name(binding_key, binding_mods);
         let [succ, iterator] = model.get_iter_from_string(iter);
 
         if (!succ) {
             throw new Error("Error updating keybinding");
         }
 
-        model.set(iterator, [0, 1], [mods, key]);
+        model.set(iterator, [0, 1], [binding_mods, binding_key]);
         settings.set_strv('shortcut-keybind', [value]);
     });
 
-    cellrend.connect('accel-cleared', function(rend, iter, key, mods) {
+    cellrend.connect('accel-cleared', function(rend, iter, binding_key, binding_mods) {
         let [succ, iterator] = model.get_iter_from_string(iter);
 
         if (!succ) {
@@ -181,78 +190,75 @@ function buildPrefsWidget() {
         model.set(iterator, [0, 1], [0, 0]);
         settings.set_strv('shortcut-keybind', []);
     });
-    
-    let col = new Gtk.TreeViewColumn({ min_width: 200 });
 
-    col.pack_end(cellrend, false);
-    col.add_attribute(cellrend, 'accel-mods', 0);
-    col.add_attribute(cellrend, 'accel-key', 1);
-    treeview.append_column(col);
+    treeview_col = new Gtk.TreeViewColumn({ min_width: 200 });
+    treeview_col.pack_end(cellrend, false);
+    treeview_col.add_attribute(cellrend, 'accel-mods', 0);
+    treeview_col.add_attribute(cellrend, 'accel-key', 1);
+    treeview.append_column(treeview_col);
     treeview.set_headers_visible(false);
-    
-    hbox.pack_start(new Gtk.Label({
+
+    settings_hbox.pack_start(new Gtk.Label({
         label: _("Key that triggers the bar to be shown."),
         use_markup: true,
         xalign: 0
     }), true, true, 0);
-    hbox.pack_end(treeview, false, true, 0);
-        
+    settings_hbox.pack_end(treeview, false, true, 0);
+
     settings.connect('changed::shortcut-keybind', function(k, b) {
-        let row = model.get(0);
-        model.set(row, [0, 1], settings.get_strv(b)); 
+        let model_row = model.get(0);
+        model.set(model_row, [0, 1], settings.get_strv(b));
     });
-    
-    settings_vbox.pack_start(hbox, false, false, 3);
-    
+
+    settings_vbox.pack_start(settings_hbox, false, false, 3);
+
 /* ++++++++++++++++++++++++++++++++++++ End: Keyboard accelerator +++++ */
 
-    let hbox = new Gtk.HBox();
+    settings_spin = Gtk.SpinButton.new_with_range(0.0,10.0,0.1);
+    settings_spin.set_value(settings.get_double('shortcut-delay'));
 
-    let spin = Gtk.SpinButton.new_with_range(0.0,10.0,0.1);
-    spin.set_value(settings.get_double('shortcut-delay'));
-
-    hbox.pack_start(new Gtk.Label({
+    settings_hbox = new Gtk.HBox();
+    settings_hbox.pack_start(new Gtk.Label({
         label: _("Delay before the bar rehides after key press."),
         use_markup: true,
         xalign: 0
     }), true, true, 0);
-    hbox.pack_end(spin, false, false, 0);
-        
+    settings_hbox.pack_end(settings_spin, false, false, 0);
+
     settings.connect('changed::shortcut-delay', function(k,b) {
-        spin.set_value(settings.get_double(b)); 
+        settings_spin.set_value(settings.get_double(b));
     });
-    spin.connect('value-changed', function(w) {
+    settings_spin.connect('value-changed', function(w) {
         settings.set_double('shortcut-delay', w.get_value());
     });
 
-    settings_vbox.pack_start(hbox, false,false, 0);
+    settings_vbox.pack_start(settings_hbox, false,false, 0);
 
-    let settings_array = [
+    settings_array = [
         ['shortcut-toggles',_("Pressing the shortcut again rehides the panel.")],
     ];
     settings_array.forEach(function (s) {
-        let hbox = new Gtk.HBox();
-        let onoff = new Gtk.Switch({active: settings.get_boolean(s[0])});
+        settings_onoff = new Gtk.Switch({active: settings.get_boolean(s[0])});
+        settings_onoff.connect('notify::active', function(w) {
+            settings.set_boolean(s[0], w.active);
+        });
+        settings.connect('changed::'+s[0], function(k,b) {
+            settings_onoff.set_active(settings.get_boolean(b));
+        });
 
-        hbox.pack_start(new Gtk.Label({
+        settings_hbox = new Gtk.HBox();
+        settings_hbox.pack_start(new Gtk.Label({
             label: s[1],
             use_markup: true,
             xalign: 0
         }), true, true, 0);
-        hbox.pack_end(onoff, false, false, 0);
+        settings_hbox.pack_end(settings_onoff, false, false, 0);
 
-        settings.connect('changed::'+s[0], function(k,b) {
-            onoff.set_active(settings.get_boolean(b)); });
-
-        onoff.connect('notify::active', function(w) {
-            settings.set_boolean(s[0], w.active);
-        });
-
-        settings_vbox.pack_start(hbox, false,false, 0);
+        settings_vbox.pack_start(settings_hbox, false,false, 0);
     });
 
     frame.pack_start(settings_vbox, true, true, 0);
-    
+
 /******************************************************************************
  ************************************** Section Intellihide *******************
  ******************************************************************************/
@@ -263,30 +269,29 @@ function buildPrefsWidget() {
         xalign: 0
     }), false, false, 0);
 
-    let settings_vbox = new Gtk.VBox({margin_left: 20, margin_top: 10, spacing: 6});
-    let settings_array = [
+    settings_vbox = new Gtk.VBox({margin_left: 20, margin_top: 10, spacing: 6});
+    settings_array = [
         ['enable-intellihide',_("Only hide panel when a window takes the space")],
         ['enable-active-window',_("Only when the active window takes the space")],
     ];
     settings_array.forEach(function (s) {
-        let hbox = new Gtk.HBox();
-        let onoff = new Gtk.Switch({active: settings.get_boolean(s[0])});
+        settings_onoff = new Gtk.Switch({active: settings.get_boolean(s[0])});
+        settings_onoff.connect('notify::active', function(w) {
+            settings.set_boolean(s[0], w.active);
+        });
+        settings.connect('changed::'+s[0], function(k,b) {
+            settings_onoff.set_active(settings.get_boolean(b));
+        });
 
-        hbox.pack_start(new Gtk.Label({
+        settings_hbox = new Gtk.HBox();
+        settings_hbox.pack_start(new Gtk.Label({
             label: s[1],
             use_markup: true,
             xalign: 0
         }), true, true, 0);
-        hbox.pack_end(onoff, false, false, 0);
+        settings_hbox.pack_end(settings_onoff, false, false, 0);
 
-        settings.connect('changed::'+s[0], function(k,b) {
-            onoff.set_active(settings.get_boolean(b)); });
-
-        onoff.connect('notify::active', function(w) {
-            settings.set_boolean(s[0], w.active);
-        });
-        
-        settings_vbox.pack_start(hbox, false,false, 0);
+        settings_vbox.pack_start(settings_hbox, false,false, 0);
     });
     frame.pack_start(settings_vbox, true, true, 0);
 
